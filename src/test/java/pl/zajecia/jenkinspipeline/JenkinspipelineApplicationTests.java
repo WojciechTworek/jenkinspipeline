cb91a69358daba43459d1a8dee49423be5b2ae9a
WojciechTworek <wojctwo445@student.polsl.pl>
2024-05-13 15:22:42 +0200

    Initial commit

---
+++ src/test/java/pl/zajecia/jenkinspipeline/JenkinspipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.zajecia.jenkinspipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinspipelineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
